299d4cd1111b9a226846833a79ef94e5d2d7315f
Ranquil <markusranki95@gmail.com>
2018-12-06 15:47:40 +0200

    Slight changes, Japanese Exp page, English Contact page
    Something is wrong with the JavaScript code on Google's side, so the
    contact form can't send me e-mail yet. Needs to be fixed.

--- scripts.js
+++ scripts.js
@@ -32,3 +32,74 @@ function redirectByLang()
 	
 	window.location.href = userLang + "/home.html";
 }
+
+function sendEmail(e, lang)
+{
+	alert("sendEmail function started");
+	
+	e.preventDefault();
+	
+	const POST_URL = "https://script.google.com/macros/s/AKfycbwxs1-lq72M_opLQt0y656wn4WnwluS0iJWKAYKuWAWkA8CwHwP/exec";
+	
+	alert("URL given");
+	
+	const postRequest =
+	{
+		name: e.target["name"].value,
+		email: e.target["email"].value,
+		subject: e.target["subject"].value,
+		body: e.target["message"].value
+	};
+	
+	alert("postRequest filled in");
+	
+	if(POST_URL)
+	{
+		alert("POST_URL has some value");
+		$.post(POST_URL, JSON.stringify(postRequest)).then(res =>
+		{
+			e.target.reset();
+			
+			alert("The form has been cleared");
+			
+			if (res)
+				switch (lang)
+				{
+					case 'fi':
+					alert("Viestisi on lähetetty eteenpäin. Kiitoksia paljon yhteydenotostasi!");
+					break;
+					
+					case 'jp':
+					alert("メッセージは届けられました。ご連絡でありがとうございました！");
+					break;
+					
+					default:
+					alert("Your message has been sent forward. Thank you very much for the contact!");
+				}
+			else
+				switch (lang)
+				{
+					case 'fi':
+					alert("Jotain meni vikaan! Tarkista, että kaikki tekstikentät on täytetty, ja että sähköpostiosoite on kirjoitettu oikein.");
+					break;
+					
+					case 'jp':
+					alert("何かは駄目になってしまいました！欄が全部詰めてあり、メールアドレスが正しく書いてあると確かめてください。");
+					break;
+					
+					default:
+					alert("Something went wrong! Please check that all text fields are filled in and the e-mail address is written correctly.");
+				}
+		});
+		
+		return true;
+		
+	}
+	
+	else
+	{
+		alert("Asetapa se POST_URL ennen kuin menet julkaisemaan keskeneräisiä sivustoja, senkin pöhkö devaaja.");
+		
+		return false;
+	}
+}
