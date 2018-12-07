e06ebe01d34254a92a16bd2f96577498fdc983a5
Ranquil <markusranki95@gmail.com>
2018-12-07 19:13:43 +0200

    Finished the Contact page (about time!) and updated it to the navigation
    Next up, image gallery! Also, at this point I really should make the navigation bar be filled via JavaScript instead of manually putting up all the links, it's really unsafe right now

--- scripts.js
+++ scripts.js
@@ -33,15 +33,11 @@ function redirectByLang()
 	window.location.href = userLang + "/home.html";
 }
 
-function sendEmail(e, lang)
+function sendEmail(e, userLang)
 {
-	alert("sendEmail function started");
-	
 	e.preventDefault();
 	
 	const POST_URL = "https://script.google.com/macros/s/AKfycbwxs1-lq72M_opLQt0y656wn4WnwluS0iJWKAYKuWAWkA8CwHwP/exec";
-	
-	alert("URL given");
 	
 	const postRequest =
 	{
@@ -51,48 +47,56 @@ function sendEmail(e, lang)
 		body: e.target["message"].value
 	};
 	
-	alert("postRequest filled in");
-	
 	if(POST_URL)
 	{
-		alert("POST_URL has some value");
-		$.post(POST_URL, JSON.stringify(postRequest)).then(res =>
+		try
 		{
-			e.target.reset();
-			
-			alert("The form has been cleared");
-			
-			if (res)
-				switch (lang)
-				{
-					case 'fi':
-					alert("Viestisi on lähetetty eteenpäin. Kiitoksia paljon yhteydenotostasi!");
-					break;
-					
-					case 'jp':
-					alert("メッセージは届けられました。ご連絡でありがとうございました！");
-					break;
-					
-					default:
-					alert("Your message has been sent forward. Thank you very much for the contact!");
-				}
-			else
-				switch (lang)
-				{
-					case 'fi':
-					alert("Jotain meni vikaan! Tarkista, että kaikki tekstikentät on täytetty, ja että sähköpostiosoite on kirjoitettu oikein.");
-					break;
-					
-					case 'jp':
-					alert("何かは駄目になってしまいました！欄が全部詰めてあり、メールアドレスが正しく書いてあると確かめてください。");
-					break;
-					
-					default:
-					alert("Something went wrong! Please check that all text fields are filled in and the e-mail address is written correctly.");
-				}
-		});
+			$.post(POST_URL, JSON.stringify(postRequest), "text").
+			then(res =>
+			{
+				
+				e.target.reset();
+				
+				if (res == "success")
+					switch (userLang)
+					{
+						case 'fi':
+						alert("Viestisi on lähetetty eteenpäin. Kiitoksia paljon yhteydenotostasi!");
+						break;
+						
+						case 'jp':
+						alert("メッセージは届けられました。ご連絡でありがとうございました！");
+						break;
+						
+						default:
+						alert("Your message has been sent forward. Thank you very much for the contact!");
+					}
+				else
+					switch (userLang)
+					{
+						case 'fi':
+						alert("Jotain meni vikaan! Tarkista, että kaikki tekstikentät on täytetty, ja että sähköpostiosoite on kirjoitettu oikein.\nVirheviesti: " + res);
+						break;
+						
+						case 'jp':
+						alert("何かは駄目になってしまいました！欄が全部詰めてあり、メールアドレスが正しく書いてあると確かめてください。\nエラーメッセージ： " + res);
+						break;
+						
+						default:
+						alert("Something went wrong! Please check that all text fields are filled in and the e-mail address is written correctly.\nError message: " + res);
+					}
+			});
 		
-		return true;
+		}
+		
+		catch(err)
+		{
+			alert("Error:\n" + err.message);
+		}
+		
+		//);
+		
+		//return true;
 		
 	}
 	
@@ -100,6 +104,6 @@ function sendEmail(e, lang)
 	{
 		alert("Asetapa se POST_URL ennen kuin menet julkaisemaan keskeneräisiä sivustoja, senkin pöhkö devaaja.");
 		
-		return false;
+		//return false;
 	}
 }
