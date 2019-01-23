e8aeb784f6f68954e42c8138d74060fff0543cd2
Ranquil <markusranki95@gmail.com>
2019-01-23 14:21:37 +0200

    Finally got the JavaScript gallery to work!

--- scripts.js
+++ scripts.js
@@ -2,29 +2,79 @@ var langCodes = ["en", "fi", "jp"];
 var langNames = ["English", "Suomi", "日本語"];
 var pageFiles = ["home.html", "#", "exp.html", "contact.html"];
 var pageIds = ["home", "gallery", "experience", "contact"];
-var pageNames =
+
+class NavigationItem
 {
-	home: ["Home", "Etusivu", "ホーム"],
-	gallery: ["Gallery", "Galleria", "ギャラリー"],
-	experience: ["Experience", "Kokemus", "経験"],
-	contact: ["Contact", "Ota yhteyttä","ご連絡"]
+	constructor (itemName, name__en, name__fi, name__jp)
+	{
+		this.itemName = itemName;
+		this.name__en = name__en;
+		this.name__fi = name__fi;
+		this.name__jp = name__jp;
+	}
+}
+
+var navItems =
+[
+	new NavigationItem("home", "Home", "Etusivu", "ホーム"),
+	new NavigationItem("gallery", "Gallery", "Galleria", "ギャラリー"),
+	new NavigationItem("exp", "Experience", "Kokemus", "経験"),
+	new NavigationItem("contact", "Contact", "Ota yhteyttä","ご連絡")
+];
+
+class GalleryImage
+{
+	constructor (fileName, name__en, name__fi, name__jp)
+	{
+		this.fileName	= fileName;
+		this.name__en	= name__en;
+		this.name__fi	= name__fi;
+		this.name__jp	= name__jp;
+	}
 };
+
 var digitalImgFiles =
 [
-	"limos",
-	"jigazou"
-]
+	new GalleryImage(
+	`limos`,
+	`Album Cover for Limos`,
+	`Levynkansi Limos-bändille`,
+	`「リモス」にアルバムカバー`),
+	new GalleryImage(
+	`jigazou`,
+	`Self-portrait`,
+	`Omakuva`,
+	`自画像`),
+	new GalleryImage(
+	`sky`,
+	`Digital Painting`,
+	`Digitaalinen maalaus`,
+	`デジタル絵画`)
+];
 var tradImgFiles =
 [
-	"whale"
-]
+	new GalleryImage(
+	`whale`,
+	`Inktober piece`,
+	`Inktober-työ`,
+	`「Inktober」作品`)
+];
 var gameArtImgFiles =
 [
 
-]
+];
 
 var url = window.location.pathname;
 var fileName = url.substring(url.lastIndexOf('/')+1);
+var lang = url.substring(url.lastIndexOf('/')-2).substr(0, 2);
+
+
+
+function pageInit()
+{
+	langLink();
+	navigationLink();
+}
 
 function langLink()
 {
@@ -44,14 +94,18 @@ function langLink()
 	document.getElementById("navlang").innerHTML = linkRow;
 }
 
-function navigationLink(lang)
+function navigationLink()
 {
 	var linkRow = "";
 	var i;
 	
-	for (i = 0; i <  pageIds.length; i++)
+	for (i = 0; i <  navItems.length; i++)
 	{
-		linkRow += "<a href='" + pageFiles[i] + "' class='nav'><span class='nav'><span id='" + pageIds[i] + "'></span></span></a>";
+		var item = navItems[i];
+		
+		var name = "name__" + lang;
+		
+		linkRow += `<a href="${item.itemName}.html" class="nav"><span class="nav"><span id="${item.itemName}">${item[name]}</span></span></a>`;
 	}
 	
 	document.getElementById("navigationBar").innerHTML = linkRow;
@@ -71,11 +125,13 @@ function redirectByLang()
 
 function gallerySetup()
 {
-	document.getElementById("digital").innerHTML = setImgs(digitalImgFiles, "digital");
+	/*document.getElementById("digital").innerHTML = setImgs(digitalImgFiles, "digital");
 	document.getElementById("trad").innerHTML = setImgs(tradImgFiles, "trad");
 	
-	document.getElementById("lightboxStorage").innerHTML = setLightboxes(digitalImgFiles, "digital") + setLightboxes(tradImgFiles, "trad");
-	/*document.getElementById("lightboxStorage").innerHTML = setLightboxes(tradImgFiles, "trad");*/
+	document.getElementById("lightboxStorage").innerHTML = setLightboxes(digitalImgFiles, "digital") + setLightboxes(tradImgFiles, "trad");*/
+	
+	document.getElementById("digital").innerHTML = setImgButtons(digitalImgFiles, "digitalImgFiles");
+	document.getElementById("trad").innerHTML = setImgButtons(tradImgFiles, "tradImgFiles");
 }
 
 function setImgs(imgs, category)
@@ -84,46 +140,95 @@ function setImgs(imgs, category)
 	
 	for (var i = 0; i < imgs.length; i++)
 	{
-		htmlCode +=	"<a href='#preview_" + imgs[i] + "'><img class='thumbnail' src='../img/gallery/" + imgs[i] + "_small.jpg'></a>";
+		htmlCode +=	`<a href="#preview_${imgs[i].fileName}"><img class="thumbnail" src="../img/gallery/${imgs[i].fileName}_small.jpg"></a>`;
 	}
 	
 	return htmlCode;
+}
+
+function setImgButtons(imgs, categoryName)
+{
+	var htmlCode = "";
+	
+	console.log(categoryName);
+	
+	for (var i = 0; i < imgs.length; i++)
+	{
+		/*var imgIndex = imgs.findIndex(function (element)
+		{
+			return element == img;
+		});*/
+		
+		htmlCode +=	`<a onclick='createLightbox("${categoryName}", ${i})'><img class="thumbnail" src="../img/gallery/${imgs[i].fileName}_small.jpg"></a>`;
+	}
+	
+	return htmlCode;
+}
+
+function createLightbox(categoryName, imgIndex)
+{
+	document.getElementById("lightboxStorage").innerHTML = "";
+	
+	var htmlCode = "";
+	var imgNameVar = "name__" + lang;
+	
+	var imgs = eval(categoryName);	/* I know, I know, I shouldn't use eval(), but I can't find any other way to do this, sorry. */
+	var img = imgs[imgIndex]
+	var imgsLengthIndex = imgs.length; imgsLengthIndex--;
+	console.log(img);
+	var imgIndexPrev = imgIndex; imgIndexPrev--;
+	var imgIndexNext = imgIndex; imgIndexNext++;
+	/*var imgPrevData = JSON.stringify(imgs[imgIndexPrev]);
+	var imgNextData = JSON.stringify(imgs[imgIndexNext]);*/
+	
+	htmlCode += `<div id="preview_${img.fileName}" class="lightbox">
+	<div class="lightbox_content h-center v-center">`;
+	if (imgIndex > 0)
+	{
+		htmlCode += `<a class="lightbox_content_previous v-center" onclick='createLightbox("${categoryName}", ${imgIndexPrev})'></a>`;
+	}
+	htmlCode += `<div class="lightbox_content_img">
+	<img src="../img/gallery/${img.fileName}_big.jpg">
+	<h5>${img[imgNameVar]}</h5>
+	</div>`;
+	if (imgIndex < imgsLengthIndex && imgs.length > 1)
+	{
+		htmlCode += `<a class="lightbox_content_next v-center" onclick='createLightbox("${categoryName}", ${imgIndexNext})'></a>`;
+	}
+	htmlCode += `<a class="lightbox_content_exit h-center v-center" onclick="document.getElementById('lightboxStorage').innerHTML = ''"></a>
+	</div>
+	</div>`;
+	
+	document.getElementById("lightboxStorage").innerHTML = htmlCode;
 }
 
 function setLightboxes(imgs, category)
 {
 	var htmlCode = "";
 	
-	console.log(`There is/are ${imgs.length} image(s) in ${category}.`);
+	var imgNameVar = "name__" + lang;
 	
 	var imgsLengthIndex = imgs.length; imgsLengthIndex--;
-	/*I have no earthly idea why I have to do this this way, but the whole browser crashes if I use "imgs.length--"*/
 	
 	for (var i = 0; i < imgs.length; i++)
 	{
-		htmlCode += `<div id='preview_${imgs[i]}' class='lightbox'>
+		var img = imgs[i];
+		
+		htmlCode += `<div id='preview_${img.fileName}' class='lightbox'>
 		<div class='lightbox_content h-center v-center'>`;
 		if (i > 0)
 		{
 			var prevImg = i; prevImg--;
-			/*I have no earthly idea why I have to do this this way, but the whole browser crashes if I use "i++"*/
-			htmlCode += `<a class='lightbox_content_previous v-center' href='#preview_${imgs[prevImg]}'></a>`;
-		}
-		else
-		{
-			/*htmlCode += `<a class='lightbox_content_dummyButton'></a>`;*/
+			htmlCode += `<a class='lightbox_content_previous v-center' href='#preview_${imgs[prevImg].fileName}'></a>`;
 		}
 		htmlCode += `<div class='lightbox_content_img'>
-		<img src='../img/gallery/${imgs[i]}_big.jpg'>
+		<img src='../img/gallery/${img.fileName}_big.jpg'>
+		<h5>${img[imgNameVar]}</h5>
 		</div>`;
 		if (i < imgsLengthIndex && imgs.length > 1)
 		{
-			var nextImg = i; nextImg++; /*Same problem here. Technology is weird*/
-			htmlCode += `<a class='lightbox_content_next v-center' href='#preview_${imgs[nextImg]}'></a>`;
-		}
-		else
-		{
-			/*htmlCode += `<a class='lightbox_content_dummyButton'></a>`;*/
+			var nextImg = i; nextImg++;
+			htmlCode += `<a class='lightbox_content_next v-center' href='#preview_${imgs[nextImg].fileName}'></a>`;
 		}
 		htmlCode += `<a class='lightbox_content_exit h-center v-center' href='#${category}'></a></div></div>`;
 	}
